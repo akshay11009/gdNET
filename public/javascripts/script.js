236b4fd4356f153c3a72b3b3bdf7ad196662789d
Sakata Gintoki <jksprt0@gmail.com>
2018-07-13 10:52:12 +0530

    node_modules/.bin/semver 	node_modules/bson/ 	node_modules/mongodb-core/ 	node_modules/mongodb/ 	node_modules/require_optional/ 	node_modules/resolve-from/ 	node_modules/saslprep/ 	node_modules/semver/ 	public/fonts/ 	public/images/index.jpeg 	public/images/profile_jethia.jpeg 	public/javascripts/styling.js 	public/stylesheets/info.css 	public/stylesheets/profile.css 	public/stylesheets/res.css 	readme.md         views/info.jade

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,3 @@
-function makeVisible() {
-	document.getElementById("forgot-container").style.display="block";
-	document.getElementById("forgot").style.display="block";
-	document.getElementsByTagName("body")[0].style.overflow="hidden";
-}
-
-function reset() {
-	document.getElementById("forgot-container").style.display="none";
-	document.getElementById("forgot").style.display="none";
-	document.getElementsByTagName("body")[0].style.overflow="auto";
-}
-
 function response() {
   var xhttp;
   xhttp=new XMLHttpRequest();
@@ -22,4 +10,60 @@ function response() {
   };
   xhttp.open("GET",'/forgot?email=' + document.getElementById("email").value, true);
   xhttp.send();
+}
+
+function signup() {
+  let elms = document.getElementById('signup-container').getElementsByTagName('input');
+  let messages = [
+    '<li>E-mail exists</li>' ,
+    '<li>Username exists</li>'
+  ];
+  let signupObj = {
+    email : elms[0].value,
+    username : elms[1].value,
+    password : elms[2].value
+  };
+  var xhttp;
+  if(elms[2].value != elms[3].value) {
+    elms[2].style.borderColor = elms[3].style.borderColor = 'red';
+    document.getElementById('error').innerHTML = '<li>Passwords do not match</li>';
+    return ;
+  }
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      if(this.responseText === 'SUCCESS')
+         window.location.href = '/user';
+      else {
+        elms[Number(this.responseText)].style.borderColor = 'red';
+        document.getElementById('error').innerHTML = messages[Number(this.responseText)];
+      }
+    }
+  };
+  xhttp.open("POST", '/confirm?signup_info=' + JSON.stringify(signupObj) , true);
+  xhttp.send();
+}
+
+function login() {
+  let elms = document.getElementById('form-login').getElementsByTagName('input');
+  let jsonObj = {
+    username : elms[0].value,
+    password : elms[1].value
+  };
+  var xhttp;
+  xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      if(this.responseText === 'SUCCESS')
+        window.location.href = '/user';
+      else {
+        elms[0].style.borderColor = elms[1].style.borderColor = 'red';
+      }
+    }
+  };
+  xhttp.open("GET",'/login?cred=' + JSON.stringify(jsonObj), true);
+  xhttp.send();
+
 }
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
-var express = require('express');
-var router = express.Router();
-var url = require('url');
-var nodemailer = require('nodemailer');
+const express = require('express');
+const router = express.Router();
+const url = require('url');
+const nodemailer = require('nodemailer');
+const MongoClient = require('mongodb').MongoClient;
+const urldb = "mongodb://localhost:27017/userd";
 
 var letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 var randomCode = "";
 var mail_sent = false;
 
+var dbo;
+
+MongoClient.connect(urldb , { useNewUrlParser: true } , function(err , db){
+	if(err)
+		throw err;
+	dbo = db.db('users');
+});
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'achha sa title' });
+});
+
+router.get('/info' , function(req , res , next){
+	res.render('info' , {title : 'disad'});
 });
 
 router.get('/signup' , function(req , res , next){
@@ -28,7 +43,6 @@ router.get('/reset' , function(req , res , next){
 router.get('/forgot' , function(req , res){
 	let email_id = url.parse(req.url , true).query.email;
 	console.log(email_id);
-
 	for(let i = 0 ; i < 10 ; i++)
 		randomCode = randomCode + letters[Math.floor(Math.random() * 36)];
 
@@ -68,6 +82,66 @@ router.get('/user' , function(req , res , next){
 	res.render('profile' , {title: 'achha sa title'});
 });
 
+router.post('/confirm' , function(req , res_confirm) {
+	let user_obj = JSON.parse(req.query.signup_info);
+	let coll = dbo.collection("users");
+	let user = {
+		username : user_obj.username,
+		email : user_obj.email,
+		password : user_obj.password,
+		skills : '',
+		score : 'nil',
+		gender : '',
+		work : '',
+		birthday : '',
+		img : ''
+	};
+	coll.findOne({ username : user_obj.username } , function(err_find , res_find) {
+		if(!res_find) {
+			coll.findOne({ email : user_obj.email } , function(err_find_email , res_find_email){
+				if(!res_find_email) {
+					coll.insertOne(user_obj , function(err_insert , res_insert){
+						res_confirm.send('SUCCESS');
+					});
+				}
+				else
+					res_confirm.send('0');
+			})
+		}
+		else 
+			res_confirm.send('1');
 
+	});
+});
+
+router.get('/login' , function(req , res_login) {
+	let credentials = JSON.parse(req.query.cred);
+	let coll = dbo.collection("users");
+	coll.findOne(credentials , function(err_find , res_find){
+		if(res_find) {
+			console.log('success');
+			res_login.send('SUCCESS');
+		}
+		else {
+			console.log('fail');
+			res_login.send('FAIL');
+		}
+	});
+});
+
+router.get('/skills' , function(req , res_skills) {
+	let colCode = req.query.colCode;
+	console.log(colCode);
+	let skills = '';
+	let job = ['Game Programmer' , 'Game Designer' , 'Game Artist' , 'Engine Programmer' , 'Physics Programmer'];
+	for(let i = 0 ; i < 5 ; i++) {
+		if(colCode[i] == '1') {
+			skills += job[i];
+			skills += ',';
+		}
+	}
+	skills[skills.length - 1] = '.';
+	res_skills.send(skills);
+});
 
 module.exports = router;
